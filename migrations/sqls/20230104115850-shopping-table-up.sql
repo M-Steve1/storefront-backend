c3318adf43811ac5cbcc1139d565034b7619e884
Modebe Stephen <modebestephen2@gmail.com>
2023-01-04 13:16:11 +0100

    Add migration files

---
+++ migrations/sqls/20230104115850-shopping-table-up.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(150),
+    lastName VARCHAR(150),
+    password VARCHAR(200)
+);
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200),
+    price integer,
+    category VARCHAR(100)
+);
+
+CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    product_id bigint REFERENCES products(id),
+    product_quantity integer,
+    user_id bigint REFERENCES users(id),
+    status VARCHAR(50)
+);
+
+CREATE TABLE order_products(
+    id SERIAL PRIMARY KEY,
+    quantity integer,
+    product_id bigint REFERENCES products(id),
+    order_id bigint REFERENCES orders(id)
+);
