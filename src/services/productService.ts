79af456d867402256fe753de9710d9d83466ba32
Modebe Stephen <modebestephen2@gmail.com>
2023-01-05 05:55:47 +0100

    Setup product service

---
+++ src/services/productService.ts
@@ -0,0 +1,20 @@
+import client from "../database";
+import { Product } from "../models/product";
+
+export class ProductService {
+    async fiveMostPopularProduct () {
+
+    }
+
+    async productsByCategory (category: string): Promise<Product[]> {
+        try {
+            const sql = 'SELECT * FROM products WHERE category=($1)';
+            const conn = await client.connect();
+            const result = await conn.query(sql, [category]);
+            conn.release();
+            return result.rows;
+        } catch (error) {
+            throw new Error(`${error}`);
+        }
+    }
+}
