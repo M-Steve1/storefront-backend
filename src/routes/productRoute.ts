ea55e14000ac5a06bcb1fadbbf2d3a50c8c77f28
Modebe Stephen <modebestephen2@gmail.com>
2023-01-08 11:22:17 +0100

    Setup token authentication middleware

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { create, getProductById, index } from "../controllers/productController";
+import { tokenAuth } from "../middleware/tokenAuth";
 
 const productRouter = express.Router();
 
 productRouter.get('/index', index);
 productRouter.get('/show/:id', getProductById);
-productRouter.post('/create', create);
+productRouter.post('/create', tokenAuth, create);
 
 export default productRouter;
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { index, getUserById, createUser, authenticate } from "../controllers/userController";
+import { tokenAuth } from "../middleware/tokenAuth";
 
 const userRouter = express.Router();
 
-userRouter.get('/index', index);
-userRouter.get('/show/:id', getUserById);
+userRouter.get('/index', tokenAuth, index);
+userRouter.get('/show/:id', tokenAuth, getUserById);
 userRouter.post('/signup', createUser);
 userRouter.post('/signin', authenticate);
 
